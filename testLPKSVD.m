90cb5c1ab6661d5d47e7067f5011a00b0377cf31
hilikliming <hilikliming@gmail.com>
2015-03-31 15:20:25 -0600

    added script for testing the effectiveness of the LP-KSVD and optimal tau, eta, etc.

---
+++ testLPKSVD.m
@@ -0,0 +1,5 @@
+% This is a script for testing and comparing the performance of the LP-KSVD
+% method versus the K-SVD and standard SVD method for manifold
+% approximation given AC data signal type
+
+
